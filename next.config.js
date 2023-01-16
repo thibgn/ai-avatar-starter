9ca94278464b79f6b10f3a993376e8e4ecf4e15c
Thibaud <thibgn@pm.me>
2023-01-16 14:37:36 +0100

    removed cache

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "x-use-cache",
+            value: "false",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig
